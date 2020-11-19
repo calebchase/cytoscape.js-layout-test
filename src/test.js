28469f7b8900c0f55e8d697c37652ab45ae4ec7b
Caleb Chase <calebchase0308@gmail.com>
2020-11-18 23:07:27 -0500

    Initial commit

--- src/test.js
+++ src/test.js
@@ -1,1 +1,43 @@
 console.log('Hello, World!');
+
+import cytoscape from 'cytoscape';
+import txt from './node.txt';
+
+document.addEventListener('DOMContentLoaded', function () {
+  var cy = (window.cy = cytoscape({
+    container: document.getElementById('cy'),
+    autounselectify: 'true',
+    style: [
+      {
+        selector: 'node',
+        css: {
+          content: ' ',
+        },
+        style: {
+          content: '',
+          'background-color': 'lightgrey',
+          shape: 'round-rectangle',
+          width: 100,
+          height: 100,
+        },
+      },
+      {
+        selector: 'edge',
+        style: {
+          'curve-style': 'bezier',
+          'target-arrow-shape': 'triangle',
+        },
+      },
+    ],
+    elements: {
+      nodes: [],
+      // edges: [{ data: { source: 'a', target: 'b' } }],
+    },
+    layout: {
+      name: 'grid',
+      spacingFactor: 0.5,
+    },
+  }));
+  console.log(JSON.parse(txt));
+  cy.add(JSON.parse(txt));
+});
